d2dfed8f7d5e8ecf3e06044bccce8671e4dde782
Ковалёв Богдан Олегович <kovalev-bo@ais-npp.local>
2022-10-31 14:43:02 +0500

    fotter_header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+  let year = new Date().getFullYear();
+
+  return (
+    <footer className="page-footer indigo darken-2">
+      <div className="footer-copyright">
+        <div className="container">
+          © {year} Copyright Text
+          <a className="grey-text text-lighten-4 right" href="#!">
+            Repo
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export { Footer };
