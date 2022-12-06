a6a6699060c70a98a08c1ab8ec6e812c2e1c9e76
Ковалёв Богдан Олегович <kovalev-bo@ais-npp.local>
2022-12-06 13:08:25 +0500

    public

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,15 @@ function Header() {
   return (
     <nav className="blue-grey darken-4">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">
+        <a
+          href="https://luckyboggy.github.io/react_pr_showcase"
+          className="brand-logo"
+        >
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">Repo</a>
+            <a href="https://github.com/luckyboggy/react_pr_showcase">Repo</a>
           </li>
         </ul>
       </div>
